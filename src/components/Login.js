f834758f97ff759c297b7e8aab8ae2646f27638c
hemanthkumar17 <hemanth17112000@gmail.com>
2020-12-25 20:10:40 +0530

    UPDATE: Login added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
- 
+import { Container, TextField, Grid, Button } from '@material-ui/core';
+
 function Login() {
   return (
-    <div>
-      Welcome to the Home Page!
-    </div>
+    <Container maxWidth="100%" style={{height:"100vh"}}>
+        <Grid container direction="column" justify="center" spacing={2} alignItems="center" style={{height:"100%"}}>
+            <Grid item>
+            <TextField id="filled-basic" label="Name" />
+            </Grid>
+            <Grid item >
+            <TextField id="filled-basic" label="Password" type="password" />
+            </Grid>
+            <Grid item >
+            <Button variant="contained" color="primary" >
+                Login
+            </Button>
+            </Grid>
+        </Grid>
+    </Container>
   );
 }
  
