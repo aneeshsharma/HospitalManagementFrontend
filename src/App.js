22e4502ed184e2284a3b5d768d894e699764de32
hemanthkumar17 <hemanth17112000@gmail.com>
2020-12-26 15:33:09 +0530

    FEATURE: Profile section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Switch, Route,  } from 'react-router-dom';
+import { Avatar, Container, Grid } from '@material-ui/core';
  
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 // import Home from './Home';
  
 function App() {
+  const NavRoutes = () => {
+    return (
+      <div>
+        <Container maxWidth = "100%">
+          <Grid container direction="row" justify="center" spacing={2} alignItems="center" style={{height: 100}}> 
+            <Grid item>
+              <Avatar src="/static/images/avatar/avatar.png" />
+            </Grid>
+            <Grid item>
+              <h1> Name Placeholder </h1>
+            </Grid>
+          </Grid>
+        </Container>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,7 +33,7 @@ function App() {
           <div className="content">
             <Switch>
               <Route exact path="/" component={Login} />
-              <Route path="/dashboard" component={Dashboard} />
+              <Route component={NavRoutes} />
               {/* <Route path="/login" component={Login} />
               <Route path="/dashboard" component={Dashboard} /> */}
             </Switch>
