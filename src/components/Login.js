21d2a4abe746e5cb87984542cce81e38c880d4b3
Anish Sharma <aneeshsharma15@outlook.com>
2020-12-26 12:37:20 +0530

    Added login button handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import { Container, TextField, Grid, Button } from '@material-ui/core';
-import { Link as RouterLink } from 'react-router-dom';
-import { MemoryRouter as Router } from 'react-router';
+import React from "react";
+import { Container, TextField, Grid, Button } from "@material-ui/core";
 
-function Login() {
-  return (
-    <Container maxWidth="100%" style={{height:"100vh"}}>
-        <Grid container direction="column" justify="center" spacing={2} alignItems="center" style={{height:"100%"}}>
-            <Grid Item> 
-              <h1>Hospital Management System</h1>
-            </Grid>
-            <Grid item>
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formData: null,
+        }
+    }
+  handleSubmit = () => {
+    this.props.history.push('/dashboard');
+  }
+    render() {
+    return (
+      <Container maxWidth="100%" style={{ height: "100vh" }}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          spacing={2}
+          alignItems="center"
+          style={{ height: "100%" }}
+        >
+          <Grid Item>
+            <h1>Hospital Management System</h1>
+          </Grid>
+          <Grid item>
             <TextField id="filled-basic" label="Name" />
-            </Grid>
-            <Grid item >
+          </Grid>
+          <Grid item>
             <TextField id="filled-basic" label="Password" type="password" />
-            </Grid>
-            <Grid item >
-            <Router>
-              <Button variant="contained" color="primary" component={RouterLink} to="/dashboard">
-                  Login
-              </Button>
-            </Router>
-            </Grid>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={this.handleSubmit}>
+              Login
+            </Button>
+          </Grid>
         </Grid>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
- 
 export default Login;
+
