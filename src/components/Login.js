b1336070b941a9a97850764c89089e4de7f231aa
Anish Sharma <aneeshsharma15@outlook.com>
2020-12-26 12:57:38 +0530

    prettier

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,52 @@
-import React from "react";
-import { Container, TextField, Grid, Button } from "@material-ui/core";
+import React from 'react'
+import { Container, TextField, Grid, Button } from '@material-ui/core'
 
 class Login extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             formData: null,
         }
     }
-  handleSubmit = () => {
-    this.props.history.push('/dashboard');
-  }
+    handleSubmit = () => {
+        this.props.history.push('/dashboard')
+    }
     render() {
-    return (
-      <Container maxWidth="100%" style={{ height: "100vh" }}>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          spacing={2}
-          alignItems="center"
-          style={{ height: "100%" }}
-        >
-          <Grid Item>
-            <h1>Hospital Management System</h1>
-          </Grid>
-          <Grid item>
-            <TextField id="filled-basic" label="Name" />
-          </Grid>
-          <Grid item>
-            <TextField id="filled-basic" label="Password" type="password" />
-          </Grid>
-          <Grid item>
-            <Button variant="contained" color="primary" onClick={this.handleSubmit}>
-              Login
-            </Button>
-          </Grid>
-        </Grid>
-      </Container>
-    );
-  }
+        return (
+            <Container maxWidth="100%" style={{ height: '100vh' }}>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    spacing={2}
+                    alignItems="center"
+                    style={{ height: '100%' }}
+                >
+                    <Grid Item>
+                        <h1>Hospital Management System</h1>
+                    </Grid>
+                    <Grid item>
+                        <TextField id="filled-basic" label="Name" />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            id="filled-basic"
+                            label="Password"
+                            type="password"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleSubmit}
+                        >
+                            Login
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        )
+    }
 }
-export default Login;
-
+export default Login
