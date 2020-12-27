ff0162cd629eea15038cdbd2c136a87c28a8e621
Anish Sharma <aneeshsharma15@outlook.com>
2020-12-27 19:07:22 +0530

    updated backend endpoints

---
+++ src/endpoints.js
@@ -0,0 +1,14 @@
+let BACKEND_ENDPOINT;
+if (!process.env.NODE_ENV === 'production') {
+    BACKEND_ENDPOINT = 'http://localhost:8080';
+} else {
+    BACKEND_ENDPOINT =
+        'https://hospital-management-backend-duc2lxyq4q-el.a.run.app';
+}
+
+console.log(BACKEND_ENDPOINT);
+console.log(process.env);
+
+export default {
+    BACKEND_ENDPOINT,
+};
